6bbb2681685078cc0154859daa1286536dbbdd94
gosiamich <gosiamichalak@poczta.fm>
2022-05-31 22:45:01 +0200

    requests commit

--- rest_api_app/views.py
+++ rest_api_app/views.py
@@ -1,8 +1,22 @@
+import requests
 from django.shortcuts import render
 from django.views import View
+
 
 # Create your views here.
 
 class Index(View):
     def get(self, request):
-        return render(request, 'rest_api_app/index.html')
+        return render(request, 'rest_api_app/index.html', {'w': books()})
+
+def books():
+    url='https://www.googleapis.com/books/v1/volumes?q=Hobbit'
+    response = requests.get(url)
+    book = response.json()
+    data = book.get('items')
+    list =[]
+    for book in data:
+        list.append(book['volumeInfo']['title'])
+
+    print(list)
+    return data
